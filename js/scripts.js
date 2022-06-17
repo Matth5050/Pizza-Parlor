821c12e396c2996c2c2030593e53380f46cacb5b
Matth5050 <matthew.f.herbert@gmail.com>
2022-06-17 09:02:21 -0700

    added second test fucntionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,40 @@
 function Pizza () {
     this.basePrice = 0;
+    this.toppings = 0;
+    this.sumPrice = 0;
 }
 
-Pizza.prototype.getPrice = function() {
-    this.basePrice += 5
+Pizza.prototype.BasePrice = function(price) {
+    this.basePrice += 5;
 }
 
-let cheesePizza = new Pizza(0);
+let a = true;
+let b = true;
 
-cheesePizza.getPrice();
-console.log(cheesePizza);
+Pizza.prototype.Toppings = function (a,b) {
+    if (a === true && b === true) {
+        this.toppings += 6;
+    } else if (a === true && b !== true) {
+        this.toppings += 4;
+    } else if (a !== true && b === true) {
+        this.toppings += 3;
+    } else {
+        this.toppings = this.toppings;
+    }
+}
+
+Pizza.prototype.finalPrice = function(final) {
+    this.sumPrice = this.basePrice + this.toppings;
+}
+
+let testPizza = new Pizza(0,0,0);
+
+function calc () {
+testPizza.BasePrice();
+testPizza.Toppings(a,b);
+testPizza.finalPrice();
+console.log(testPizza.sumPrice);
+}
+
+calc();
 
